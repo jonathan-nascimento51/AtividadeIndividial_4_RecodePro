19d8b62a71e431ee850da894d7fbae0cfd8f588d
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2022-10-24 13:36:40 -0300

    renomeando e refatorando classe CLientRepository para TicketRepository
    renomeando e refatorando classe CLientRepository para TicketRepository devido a mudanças no projeto

--- spring-backend/src/main/java/br/com/api/repositorys/TicketRepository.java
+++ spring-backend/src/main/java/br/com/api/repositorys/TicketRepository.java
@@ -3,9 +3,10 @@ package br.com.api.repositorys;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.api.model.Client;
+import br.com.api.model.Ticket;
+
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
 }
